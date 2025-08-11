0206623906d3e8f1173b53163e06c66fe1df89b2
Mohamed El Moctar Aloueimin (Med Sidatt) <mdaloueimine@gmail.com>
2025-08-11 18:00:59 +0000

    Updating code

--- mahdhara/accounts/migrations/0002_user_role.py
+++ mahdhara/accounts/migrations/0002_user_role.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0002_create_super_user'),
+        ('accounts', '0001_initial'),
     ]
 
     operations = [
